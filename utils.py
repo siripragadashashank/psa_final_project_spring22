1badbccdcc213f1016567e5a572d4310d3fd1b1a
Mayannk Kumaar <kumaar.m@northeastern.edu>
2022-04-09 20:33:31 -0400

    adding ui

--- utils.py
+++ utils.py
@@ -29,3 +29,27 @@ def load(filename):
 
     return states
 
+def ui(state):
+    
+    pos = list(state)
+    
+    for i in range(len(pos)):
+        if pos[i] == 1: pos[i] = "X"
+        if pos[i] == 2: pos[i] = "O"
+        if pos[i] == 0: pos[i] = " "
+
+    row1, row2, row3 = pos[0:3], pos[3:6], pos[6:9]
+
+    print("  ", row1[0], "  |  ", row1[1], "  |  ", row1[2])
+    print('----------------------')
+    print("  ", row2[0], "  |  ", row2[1], "  |  ", row2[2])
+    print('----------------------')
+    print("  ", row3[0], "  |  ", row3[1], "  |  ", row3[2])
+
+    print("\n\nIndices\n\n")
+
+    print("  ", "0", "  |  ", "1", "  |  ", "2")
+    print('----------------------')
+    print("  ", "3", "  |  ", "4", "  |  ", "5")
+    print('----------------------')
+    print("  ", "6", "  |  ", "7", "  |  ", "8")
