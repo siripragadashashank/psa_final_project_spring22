b2a043e9d457a4068e20f1f550838e0b8704502e
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-04-11 14:13:58 -0400

    update game.py; enabled logging state info

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import os.path
+import logging
 from utils import save, load, ui
 from argparse import ArgumentParser
-from menace import menace_vs_human, menace_train
+from menace import menace_vs_human #, menace_train
 from states import isequal, inverse_transform_action
 from states import check_win, next_state, remove_duplicates, set_possible_moves
 
@@ -15,61 +16,61 @@ def init_states():
 	states6, states7, states8, states9 = [], [], [], []
 
 	if os.path.exists('./model.dat'):
-		print('Loading model.dat \n')
+		logging.info('Loading model.dat \n')
 		states = load('./model.dat')
 
 	else:
-		print("Creating all possible states")
-		print("No of first possible move states: ", len(states1))
-		print('#######################################################\n')
+		logging.info("Creating all possible states")
+		logging.info("No of first possible move states: {}".format(len(states1)))
+		logging.info('#######################################################')
 
-		next_state(states1,states2,2)
-		print("No. of next possible states: ", len(states2))
+		next_state(states1, states2, 2)
+		logging.info("No. of next possible move states: {}".format(len(states2)))
 		states2 = remove_duplicates(states2)
-		print("Reduced no. of states after eliminating duplicates: ", len(states2))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states2)))
+		logging.info('#######################################################')
 
-		next_state(states2,states3,1)
-		print("No. of next possible states: ", len(states3))
+		next_state(states2, states3, 1)
+		logging.info("No. of next possible move states: {}".format(len(states3)))
 		states3 = remove_duplicates(states3)
-		print("Reduced no. of states after eliminating duplicates: ", len(states3))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states3)))
+		logging.info('#######################################################')
 
-		next_state(states3,states4,2)
-		print("No. of next possible states: ", len(states4))
+		next_state(states3, states4, 2)
+		logging.info("No. of next possible move states: {}".format(len(states4)))
 		states4 = remove_duplicates(states4)
-		print("Reduced no. of states after eliminating duplicates: ", len(states4))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states4)))
+		logging.info('#######################################################')
 		
-		next_state(states4,states5,1)
-		print("No. of next possible states: ", len(states5))
+		next_state(states4, states5, 1)
+		logging.info("No. of next possible move states: {}".format(len(states5)))
 		states5 = remove_duplicates(states5)
-		print("Reduced no. of states after eliminating duplicates: ", len(states5))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states5)))
+		logging.info('#######################################################')
 		
-		next_state(states5,states6,2)
-		print("No. of next possible states: ", len(states6))
+		next_state(states5, states6, 2)
+		logging.info("No. of next possible move states: {}".format(len(states6)))
 		states6 = remove_duplicates(states6)
-		print("Reduced no. of states after eliminating duplicates: ", len(states6))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states6)))
+		logging.info('#######################################################')
 		
-		next_state(states6,states7,1)
-		print("No. of next possible states: ", len(states7))
+		next_state(states6, states7, 1)
+		logging.info("No. of next possible move states: {}".format(len(states7)))
 		states7 = remove_duplicates(states7)
-		print("Reduced no. of states after eliminating duplicates: ", len(states7))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states7)))
+		logging.info('#######################################################')
 		
-		next_state(states7,states8,2)
-		print("No. of next possible states: ", len(states8))
+		next_state(states7, states8, 2)
+		logging.info("No. of next possible move states: {}".format(len(states8)))
 		states8 = remove_duplicates(states8)
-		print("Reduced no. of states after eliminating duplicates: ", len(states8))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states8)))
+		logging.info('#######################################################')
 		
-		next_state(states8,states9,1)
-		print("No. of next possible states: ", len(states9))
+		next_state(states8, states9, 1)
+		logging.info("No. of next possible move states: {}".format(len(states9)))
 		states9 = remove_duplicates(states9)
-		print("Reduced no. of states after eliminating duplicates: ", len(states9))
-		print('#######################################################\n')
+		logging.info("Reduced no. of states after eliminating duplicates: {}".format(len(states9)))
+		logging.info('#######################################################')
 		
 		states =	states1 + states2 + states3 + states4 + \
 					states5 + states6 + states7 + states8 + \
@@ -90,6 +91,9 @@ def init_states():
 
 
 if __name__ == '__main__':
+	
+	logging.basicConfig(filename='game.log', level=logging.INFO, filemode='w')
+
 	parser = ArgumentParser()
 	parser.add_argument('--mode', help='Play against menace or Train it')
 	parser.add_argument('--iterations', type=int, help='Number of iterations for training')
@@ -98,7 +102,7 @@ if __name__ == '__main__':
 	states = init_states()
 
 	if args.mode == 'play':
-		menace_vs_human(states)
+		menace_vs_human(states, 1)
 
 	if args.mode == 'train':
 		menace_train(states, args.iterations)
